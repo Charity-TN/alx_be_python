d6adfe7cbd57a83093387582b41c7c841327eadf
Charity Thuo <charitynthuo@gmail.com>
2025-01-20 12:16:39 +0300

    Modified the script by printing out the values of the variables

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,6 +3,11 @@ P = 1000 #principal (representing $1000)
 R = 0.05 #rate  (representing 5% annual interest rate)
 T = 3    #time (representing 3 years)
 
+#Print these variables
+print(f"P (Principal): {P}")
+print(f"R (Rate): {R}")
+print(f"T (Time): {T}")
+
 # Calculate the simple interest
 I = P * R * T
 
