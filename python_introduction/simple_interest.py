b802726babf5ecf66a66a90c70b1e2ab9ddb0dad
Charity Thuo <charitynthuo@gmail.com>
2025-01-20 12:06:23 +0300

    Added the file and did a simple interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Calculate the simple interest using the following variables
+P = 1000 #principal (representing $1000)
+R = 0.05 #rate  (representing 5% annual interest rate)
+T = 3    #time (representing 3 years)
+
+# Calculate the simple interest
+I = P * R * T
+
+#Print the results
+print(f"The simple interest is : {I}")
