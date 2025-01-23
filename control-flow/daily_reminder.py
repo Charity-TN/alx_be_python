4fef0bfffdf2a1bea47cb1388e8729496ae1914f
Charity Thuo <charitynthuo@gmail.com>
2025-01-23 23:54:55 +0300

    Modified the script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,32 +1,32 @@
 def main():
-    task = input("Enter your task: ").strip()
-    priority = input("Priority (high,medium,low): ").strip().lower()
-    time_bound = input("Is it time-bound? (yes or no): ").strip().lower()
+    while True:
+        task = input("Enter your task: ").strip()
+        priority = input("Priority (high,medium,low): ").strip().lower()
+        time_bound = input("Is it time-bound? (yes or no): ").strip().lower()
 
-    reminder = ""
+        reminder = ""
 
-    match priority:
-        case "high":
-            reminder = f"Reminder: {task} is a high-priority task."
-        case "medium":
-            reminder = f"Reminder: {task} is a medium-priority task.Plan to address it soon."
-        case "low":
-            reminder = f"Note: {task} is a low-priority task. Consider completing it when you have free time."
-        case _:
-                print("Invalid priority. Plaese enter high, medium or low.")
+        match priority:
+            case "high":
+                reminder = f"Reminder: {task} is a high-priority task."
+            case "medium":
+                reminder = f"Reminder: {task} is a medium-priority task.Plan to address it soon."
+            case "low":
+                reminder = f"Note: {task} is a low-priority task. Consider completing it when you have free time."
+            case _:
+                    print("Invalid priority. Plaese enter high, medium or low.")
 
-                return
+                    continue
 
-    if time_bound == "yes":
-        reminder += "This task is time-sensitive and requires immediate attention."
-    elif time_bound == "no":
-        reminder += "This task is not time-sensitive."
-    else:
-        print("Invalid input for time-sensitivity. Please enter yes or no.")
+        if time_bound == "yes":
+            reminder += "This task is time-sensitive and requires immediate attention."
+        elif time_bound == "no":
+            reminder += "This task is not time-sensitive."
+        else:
+            print("Invalid input for time-sensitivity. Please enter yes or no.")
     
-        return
-    
-    print(f"\n{reminder}")
-
+            continue
+        print(f"\n{reminder}")
+        break
 if __name__ == "__main__":
     main()
