c80385b07c8b51f15b6baf44c5e7abd9423caede
Charity Thuo <charitynthuo@gmail.com>
2025-02-21 18:19:35 +0300

    Modified the script

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,19 +1,19 @@
 #Using match case statements to handle arithmetic operations
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operator = input("Choose the operation (+, -, *, /): ")
+operator = input("Choose the operation (+, -, *, /): ").strip()
 
 match operator:
-    case '+':
+    case "+":
         result = num1 + num2
         print(f"The result is {result}")
-    case '-':
+    case "-":
         result = num1 - num2
         print(f"The result is {result}")    
-    case '*':
+    case "*":
         result = num1 * num2
         print(f"The result is {result}")    
-    case '/':
+    case "/":
         if num2 == 0: #Division by zero check
               print("Cannot divide by zero")
         else:
